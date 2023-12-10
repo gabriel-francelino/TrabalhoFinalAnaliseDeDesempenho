4a6b312ef1311fe8d4df34fd87372519a129c258
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-12-10 15:06:26 -0300

    removendo comentarios e identando codigo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
     Proposta do Trabalho:
-        * -- Trabalho 2 -- 
+        * -- Trabalho 2 --
 
         ! Rede de um Trabalho Remoto
         (Várias chamadas dos funcionários)
@@ -10,17 +10,17 @@
         O tempo médio entre os pacotes depende do CODEC (de audio e vídeo da aplicação)
             G711u/a - 50 pacotes por segundo (64Kbps - 160 bytes por pacote)
             160 bytes + 8 bytes (UDP) + 20 bytes (IP) = 188 bytes
-        Link de 16bps 
+        Link de 16bps
         O intervalo entre ligações também é exponencial.
 
-        Quantas pessoas são necessárias no link ao mesmo tempo para obter 
+        Quantas pessoas são necessárias no link ao mesmo tempo para obter
         ocupação de (60%, 80%, 90% e 99%)
 
         ? Depois disso, calcular os mesmos valores do último simulador:
 
-        Ocupação, máximo de fila, E[N], E[W] e Erro de Little. 
+        Ocupação, máximo de fila, E[N], E[W] e Erro de Little.
         Tempo de Simulação = 864.000 = 10 dias
-        4 cenários - 
+        4 cenários -
             Ocupação Aprox 60%;
             Ocupação Aprox 80%;
             Ocupação Aprox 90%;
@@ -36,7 +36,7 @@
 
         Gráficos de Erro de Little
             Erro Little |E[N]-E[W]*Lambda| Aprox 0
-            Medir erro a cada 10 segundos exatos. 
+            Medir erro a cada 10 segundos exatos.
             Um gráfico para cada cenário -> Erro x Tempo
 */
 
@@ -59,21 +59,24 @@ using namespace std;
 // --------------------------------------------------------------------------
 
 // Struct com os parâmetros da simulação
-typedef struct {
+typedef struct
+{
     double media_chegada;
     double media_servico;
     double tempo_simulacao;
 } parametros;
 
 // Struct para o Calculo de Little
-typedef struct {
+typedef struct
+{
     unsigned long int no_eventos;
     double tempo_anterior;
     double soma_areas;
 } little;
 
 // Struct com os valores das taxas de chegada para as ocupações
-typedef struct {
+typedef struct
+{
     double taxa60;
     double taxa80;
     double taxa90;
@@ -83,18 +86,20 @@ typedef struct {
 // --------------------------------------------------------------------------
 
 // Calcula a taxa de chegada de acordo com a ocupação passada
-double calcular_taxa(double ocupacao) {
-   double bytesPorPessoaPorSegundo = 9400;
-   double bitsPorPessoaPorSegundo = bytesPorPessoaPorSegundo * 8;
-   double gigabitsPorPessoaPorSegundo = bitsPorPessoaPorSegundo / 1000000000;
-   double nPessoas = ocupacao / gigabitsPorPessoaPorSegundo;
-   double nPessoasPorSegundo = (nPessoas / 2) / 60;
+double calcular_taxa(double ocupacao)
+{
+    double bytesPorPessoaPorSegundo = 9400;
+    double bitsPorPessoaPorSegundo = bytesPorPessoaPorSegundo * 8;
+    double gigabitsPorPessoaPorSegundo = bitsPorPessoaPorSegundo / 1000000000;
+    double nPessoas = ocupacao / gigabitsPorPessoaPorSegundo;
+    double nPessoasPorSegundo = (nPessoas / 2) / 60;
 
-   return nPessoasPorSegundo;
+    return nPessoasPorSegundo;
 }
 
 // Define a taxa de chegada para as 4 ocupações
-void definir_ocupacoes(ocupacoes *ocupacoes) {
+void definir_ocupacoes(ocupacoes *ocupacoes)
+{
     // Para 60% de ocupação, 600Mbps em 1Gbps
     ocupacoes->taxa60 = calcular_taxa(0.6);
     // Para 80% de ocupação, 800Mbps em 1Gbps
@@ -108,9 +113,11 @@ void definir_ocupacoes(ocupacoes *ocupacoes) {
 // ------------------------------------------------------------------------------------------------------------------------------
 
 // Função para definir os parâmetros da simulação passando o cenário desejado
-void define_parametros(parametros *params, int cenario, ocupacoes ocupacoes){
+void define_parametros(parametros *params, int cenario, ocupacoes ocupacoes)
+{
     printf("\n");
-    switch (cenario) {
+    switch (cenario)
+    {
     case 1: // Ocupação 60%
         puts("Cenário 1 (60%):");
         printf("-> Taxa de chegada de pessoas (s): %.4f", ocupacoes.taxa60);
@@ -147,7 +154,8 @@ void define_parametros(parametros *params, int cenario, ocupacoes ocupacoes){
 // --------------------------------------------------------------------------
 
 // Função que inicia os valores para o cálculo de little
-void inicia_little(little *l) {
+void inicia_little(little *l)
+{
     l->no_eventos = 0;
     l->tempo_anterior = 0.0;
     l->soma_areas = 0.0;
@@ -157,22 +165,26 @@ void inicia_little(little *l) {
 
 // Funções para o cálculo do tempo exponencial
 
-double uniforme() {
-    double u = rand() / ((double) RAND_MAX + 1);
+double uniforme()
+{
+    double u = rand() / ((double)RAND_MAX + 1);
     u = 1.0 - u;
     return u;
 }
 
-double gerar_tempo(double media) {
-    return (-1.0/media) * log(uniforme());
+double gerar_tempo(double media)
+{
+    return (-1.0 / media) * log(uniforme());
 }
 
 // --------------------------------------------------------------------------
 
 // Funções Auxiliares
 
-double min(double d1, double d2) {
-    if(d1 < d2) return d1;
+double min(double d1, double d2)
+{
+    if (d1 < d2)
+        return d1;
     return d2;
 }
 
@@ -180,42 +192,48 @@ double min(double d1, double d2) {
 
 // Funções para escrever os resultados em arquivos CSV
 
-void resultado_csv(int cenario, double ocupacao_simulada, double e_n, double e_w, int max_fila) {
+void resultado_csv(int cenario, double ocupacao_simulada, double e_n, double e_w, int max_fila)
+{
     // Cria um arquivo para os resultados
     char nome_arquivo[20];
     sprintf(nome_arquivo, "res/results_%d.csv", cenario);
     FILE *arquivo = std::fopen(nome_arquivo, "w");
-    if (arquivo == nullptr) {
+    if (arquivo == nullptr)
+    {
         perror("Erro ao gerar o arquivo");
         exit(1);
     }
-    // Escreve as medidas de validação no arquivo    
+    // Escreve as medidas de validação no arquivo
     fprintf(arquivo, "Cenario,Ocupação Simulada,E[N],E[W],Fila Máxima\n");
     fprintf(arquivo, "%d, %.6lf, %.6lf, %.6lf, %d\n", cenario, ocupacao_simulada, e_n, e_w, max_fila);
     fclose(arquivo);
 }
 
-void erro_csv(int cenario, int n_erros, double erros_little[]) {
+void erro_csv(int cenario, int n_erros, double erros_little[])
+{
     // Cria um arquivo para os erros do cenário
     char nome_arquivo[20];
     sprintf(nome_arquivo, "res/erros_%d.csv", cenario);
     FILE *arquivo = fopen(nome_arquivo, "w");
-    if (arquivo == nullptr) {
+    if (arquivo == nullptr)
+    {
         perror("Erro ao gerar o arquivo");
         exit(1);
     }
-    // Escreve o cabeçalho para o vetor de erros de Little    
+    // Escreve o cabeçalho para o vetor de erros de Little
     fprintf(arquivo, "Tempo,Erro Little\n");
-    // Escreve os valores do vetor de erros de Little    
-    for (int i = 0; i < n_erros; i++) {
-        fprintf(arquivo, "%.2lf, %.20lf\n", (i+1) * 10.0, erros_little[i]);
+    // Escreve os valores do vetor de erros de Little
+    for (int i = 0; i < n_erros; i++)
+    {
+        fprintf(arquivo, "%.2lf, %.20lf\n", (i + 1) * 10.0, erros_little[i]);
     }
     fclose(arquivo);
 }
 
 // --------------------------------------------------------------------------
 
-int main() {
+int main()
+{
     int seed = time(NULL);
     srand(seed);
     printf("\nSeed: %d\n", seed);
@@ -232,7 +250,7 @@ int main() {
     definir_ocupacoes(&ocupacoes);
 
     // ------------------------------------
-    
+
     // Parâmetros da simulação
     parametros params;
     define_parametros(&params, cenario, ocupacoes);
@@ -244,7 +262,7 @@ int main() {
     little e_w_chegada; // E[W] Chegada
     little e_w_saida;   // E[W] Saída
     inicia_little(&e_n);
-    inicia_little(&e_w_chegada); 
+    inicia_little(&e_w_chegada);
     inicia_little(&e_w_saida);
 
     // ------------------------------------
@@ -271,46 +289,49 @@ int main() {
     double erros_little[n_erros];
 
     // ------------------------------------
-    
+
     // Variáveis de interesse para a simulação
-    
+
     double soma_ocupacao = 0.0;
     unsigned long int fila = 0;
     int max_fila = 0;
 
     // ------------------------------------
 
-    // Árvore Min Heap 
+    // Árvore Min Heap
 
     MinHeap meuHeap;
 
-    while (tempo_decorrido < params.tempo_simulacao) {
-        
+    while (tempo_decorrido < params.tempo_simulacao)
+    {
+
         Conexao conexao_chegada_pacote = meuHeap.mostrarMenorConexao();
         chegada_pacote = conexao_chegada_pacote.tempo_chegada_pacote;
 
         // Determinar o próximo evento
         tempo_decorrido = min(nova_conexao, min(tempo_coleta, min(chegada_pacote, saida_pacote)));
 
-        if (tempo_decorrido == nova_conexao) {
+        if (tempo_decorrido == nova_conexao)
+        {
             double tempo_duracao = tempo_decorrido + gerar_tempo((1.0 / params.media_servico));
             Conexao conexao = {tempo_duracao, nova_conexao};
             meuHeap.adicionarConexao(conexao);
             nova_conexao = tempo_decorrido + gerar_tempo((1.0 / intervalo_conexao));
-
-        } else if (tempo_decorrido == chegada_pacote) {
+        }
+        else if (tempo_decorrido == chegada_pacote)
+        {
 
             conexao_chegada_pacote.tempo_chegada_pacote += 0.020;
             meuHeap.removerConexao();
-            if (conexao_chegada_pacote.tempo_duracao > conexao_chegada_pacote.tempo_chegada_pacote) {
+            if (conexao_chegada_pacote.tempo_duracao > conexao_chegada_pacote.tempo_chegada_pacote)
+            {
                 meuHeap.adicionarConexao(conexao_chegada_pacote);
             }
 
-            if(!fila) {
+            if (!fila)
+            {
                 saida_pacote = tempo_decorrido + atraso_transmissao;
-                soma_ocupacao += atraso_transmissao; // ! É isso mesmo?
-                // ! Acho que está entrando menas vezes nos eventos do que deveria
-                // printf("soma ocupacao: %lf, tempo_duracao: %lf\n", soma_ocupacao, tempo_decorrido);
+                soma_ocupacao += atraso_transmissao;
             }
             fila++;
             max_fila = max_fila > fila ? max_fila : fila;
@@ -324,17 +345,21 @@ int main() {
             e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
             e_w_chegada.no_eventos++;
             e_w_chegada.tempo_anterior = tempo_decorrido;
+        }
+        else if (tempo_decorrido == saida_pacote)
+        {
 
-        } else if (tempo_decorrido == saida_pacote) {
-            
             fila--;
-            if(fila) {
+            if (fila)
+            {
                 saida_pacote = tempo_decorrido + atraso_transmissao;
                 soma_ocupacao += atraso_transmissao;
-            } else  {
+            }
+            else
+            {
                 saida_pacote = DBL_MAX;
             }
-            
+
             // Cálculo Little -> E[N]
             e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
             e_n.no_eventos--;
@@ -344,8 +369,9 @@ int main() {
             e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
             e_w_saida.no_eventos++;
             e_w_saida.tempo_anterior = tempo_decorrido;
-
-        } else if (tempo_decorrido == tempo_coleta) {
+        }
+        else if (tempo_decorrido == tempo_coleta)
+        {
 
             e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
             e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
@@ -358,18 +384,20 @@ int main() {
             double e_w_calculo = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
             double lambda = e_w_chegada.no_eventos / tempo_decorrido;
             double erro_little = e_n_calculo - (lambda * e_w_calculo);
-            if(erro_little < 0) {
-                erro_little = (-1.0)*erro_little;
-            }           
+            if (erro_little < 0)
+            {
+                erro_little = (-1.0) * erro_little;
+            }
             erros_little[indice_erro] = erro_little;
             indice_erro++;
 
             tempo_coleta += INTERVALO_COLETA;
-
-        } else {
+        }
+        else
+        {
             printf("Evento inválido!\n");
             break;
-        }        
+        }
     }
 
     // --------------------------------------------------------------------------
@@ -380,7 +408,7 @@ int main() {
     // --------------------------------------------------------------------------
 
     // Informações finais sobre a simulação
-    puts("\n---- Validação da Simulação ----"); 
+    puts("\n---- Validação da Simulação ----");
     printf("-> Tamanho Máximo da Fila: %d\n", max_fila);
     double ocupacao = soma_ocupacao / tempo_decorrido;
     printf("-> Ocupação Simulada: %lF\n", ocupacao);
@@ -389,8 +417,8 @@ int main() {
     double lambda = e_w_chegada.no_eventos / tempo_decorrido;
     printf("-> E[N] = %lF\n", e_n_calculo);
     printf("-> E[W] = %lF\n", e_w_calculo);
-    double erro_little_final =  e_n_calculo - (lambda * e_w_calculo);
-    erro_little_final = erro_little_final < 0 ? (-1)*erro_little_final : erro_little_final;
+    double erro_little_final = e_n_calculo - (lambda * e_w_calculo);
+    erro_little_final = erro_little_final < 0 ? (-1) * erro_little_final : erro_little_final;
     printf("-> Erro de Little Final: %.20lF\n\n", erro_little_final);
 
     // --------------------------------------------------------------------------
